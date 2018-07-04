453cbf98d7cfb0f6f196e5f57321f8d30ac932fe
balasubramanyam-atmakur <balasubramanyam.atmakur@allianz.com>
2018-07-04 15:56:55 +0530

    Intial commit with HelloWorld programe

---
+++ src/com/masterjavaonline/ds/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.masterjavaonline.ds;
+
+/**
+ * @author Master JAVA Online
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+	}
+
+}
